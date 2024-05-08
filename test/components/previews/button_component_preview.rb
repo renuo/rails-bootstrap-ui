c61110c1c51b4fda0913d59d53457cf2b9e3d556
Etienne Kyburz <etienne.kyburz@renuo.ch>
2024-05-08 16:19:40 +0200

    Add more options to playground

--- test/components/previews/button_component_preview.rb
+++ test/components/previews/button_component_preview.rb
@@ -1,9 +1,15 @@
 class ButtonComponentPreview < Lookbook::Preview
-  def playground
-    render "buttons/playground"
+  # Interactive playground for experimenting with different button styles and states.
+  # @param title "Button title"
+  # @param theme select { choices: [primary, secondary, danger, warning, info, light, dark, link] } "Default bootstrap themes"
+  # @param state select { choices: [default, active, disabled] } "Button state"
+  # @param outline select { choices: [true, false] } "Outline button"
+  # @param block select { choices: [true, false] } "Block button"
+  def playground(title: "Placeholder", theme: "primary", state: "default", outline: false, block: false)
+    render "buttons/playground", title: title, theme: theme, state: state, outline: outline, block: block
   end
 
   def themes
-    render ButtonComponent.new(text: "Launch spaceship", icon: "rocket")
+    render "buttons/themes"
   end
 end
