3d030d9178eb0a76962484e6ddab72099dcaecf3
Etienne Kyburz <etienne.kyburz@renuo.ch>
2024-05-14 12:11:23 +0200

    Add more tests

--- test/system/pages_test.rb
+++ test/system/pages_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class PagesTest < ActionView::TestCase
-  test "visit the themes page" do
+  test "visit the buttons themes page" do
     render template: "components/buttons/_themes", layout: "layouts/application"
 
     assert_select "button", text: "Primary"
@@ -9,10 +9,25 @@ class PagesTest < ActionView::TestCase
     assert_select "button.active", text: "Active toggle button"
   end
 
-  test "visit the playground page" do
-    render template: "components/buttons/_playground", layout: "layouts/application", locals: { title: "Primary", theme: "primary", state: "default", outline: false, block: false }
+  test "visit the buttons playground page" do
+    render template: "components/buttons/_playground", layout: "layouts/application", locals: {title: "Primary", theme: "primary", state: "default", outline: false, block: false}
 
     assert_select "button.btn-primary", text: "Primary"
     assert_select "button.btn-primary.active", count: 0
+  end
+
+  test "visit the cards themes page" do
+    render template: "components/cards/_themes", layout: "layouts/application"
+
+    assert_select "div.card"
+    assert_select "div.card-body"
+    assert_select "ul.list-group"
+  end
+
+  test "visit the cards playground page" do
+    render template: "components/cards/_playground", layout: "layouts/application", locals: {title: "Card Title", list_group: false, image: false, body: true, background_color: "default"}
+
+    assert_select "div.card", text: /Card Title/
+    assert_select "div.card-body", count: 2
   end
 end
