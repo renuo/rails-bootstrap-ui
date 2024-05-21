4ccb55ca14c0e9c7456f7c5ed6190760ccc51dbe
Etienne Kyburz <etienne.kyburz@renuo.ch>
2024-05-21 11:36:42 +0200

    Change eslinter

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,19 +1,7 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
   connect() {
-    this.element.textContent = "Hello World!"
+    this.element.textContent = 'Hello World!';
   }
 }
-
-// app/javascript/application.js
-
-// Example of unused variable
-// const unusedVar = 42;
-
-// Example of missing semicolon
-console.log('Hello, world')
-
-// Example of console statement (if using Airbnb style guide)
-console.log('This is a test');
-
