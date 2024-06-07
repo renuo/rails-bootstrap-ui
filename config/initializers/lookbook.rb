3509c0f3bb6d7ce125811a8814a710025e9c8529
Etienne Kyburz <etienne.kyburz@renuo.ch>
2024-06-07 11:31:20 +0200

    Linter fix

--- config/initializers/lookbook.rb
+++ config/initializers/lookbook.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 Rails.application.config.lookbook.project_name = 'Rails Bootstrap Ui'
 
 Rails.application.config.lookbook.debug_menu = true
@@ -15,17 +17,17 @@ Rails.application.config.lookbook.default_preview_layout = 'application'
 Rails.application.config.lookbook.preview_paths = ['app/components/previews']
 
 Lookbook.add_panel('assets', 'panels/assets', {
-  label: 'Assets',
-  locals: lambda do |data|
-    preview_path = data.preview.lookup_path
-    preview_name = File.basename(preview_path, '.rb')
-    base_name = preview_name.gsub('_preview', '')
-    asset_files = [
-      Rails.root.join("app/javascript/controllers/#{base_name}_controller.js"),
-      Rails.root.join("app/assets/stylesheets/_#{base_name}.scss")
-    ]
-    existing_files = asset_files.select { |path| File.exist?(path) }
-    assets = existing_files.map { |path| Pathname.new(path) }
-    { assets: assets }
-  end
-})
+                     label: 'Assets',
+                     locals: lambda do |data|
+                               preview_path = data.preview.lookup_path
+                               preview_name = File.basename(preview_path, '.rb')
+                               base_name = preview_name.gsub('_preview', '')
+                               asset_files = [
+                                 Rails.root.join("app/javascript/controllers/#{base_name}_controller.js"),
+                                 Rails.root.join("app/assets/stylesheets/_#{base_name}.scss")
+                               ]
+                               existing_files = asset_files.select { |path| File.exist?(path) }
+                               assets = existing_files.map { |path| Pathname.new(path) }
+                               { assets: assets }
+                             end
+                   })
