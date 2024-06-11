02edeb3be1b6bea8ca5a4269e2dc113e32d67ab6
Etienne Kyburz <etienne.kyburz@renuo.ch>
2024-06-11 11:37:01 +0200

    Create previews to showcase utility classes

--- app/components/previews/utilities_component_preview.rb
+++ app/components/previews/utilities_component_preview.rb
@@ -4,7 +4,17 @@ class UtilitiesComponentPreview < Lookbook::Preview
   include LookbookHelper
   # Interactive playground for experimenting with different button styles and states.
   # @source ../../../app/views/components/utilities/_playground.html.erb
-  def playground
-    render 'components/utilities/playground'
+  # @param position select { choices: [relative, absolute, sticky, fixed, static] } "Bootstrap position classes"
+  # @param responsive_classes select { choices: [sm, md, lg, xl, xxl] } "Bootsrap responsive classes"
+  # @param arrangement select { choices: [top, right, bottom, left] } "Bootstrap arrangement classes"
+  # @param arrangement_values select { choices: [0, 50, 100] } "Bootstrap arrangement values"
+  def playground(position: 'relative', responsive_classes: 'sm', arrangement: 'top', arrangement_values: '0')
+    render 'components/utilities/playground', position:, responsive_classes:, arrangement:, arrangement_values:
+  end
+
+  # @source ../../../app/views/components/utilities/_themes.html.erb
+
+  def themes
+    render 'components/utilities/themes'
   end
 end
