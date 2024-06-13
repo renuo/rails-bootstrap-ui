cf3a51ff6dce29aa9eea6d461e81e100a5780609
Etienne Kyburz <etienne.kyburz@renuo.ch>
2024-06-13 08:10:31 +0200

    Refactoring stimulus controller

--- app/javascript/controllers/disappearing_alert_controller.js
+++ app/javascript/controllers/disappearing_alert_controller.js
@@ -1,31 +1,19 @@
-import {Controller} from '@hotwired/stimulus';
+import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
-    static targets = ['alert', 'alertTemplate'];
+  static targets = ['alert', 'alertTemplate'];
 
-    connect() {
-        this.bsAlert = bootstrap.Alert.getOrCreateInstance(this.element);
-    }
+  connect() {
+    this.bsAlert = bootstrap.Alert.getOrCreateInstance(this.element);
+  }
 
-    liveAlert() {
-        const alertPlaceholder = this.alertTarget;
-        const appendAlert = () => {
-            const wrapper = document.createElement('div');
-            wrapper.innerHTML = [
-                `<div class="alert alert-info alert-dismissible fade show" 
-              role="alert"
-              data-controller='disappearing-alert'
-              data-action='animationend->disappearing-alert#dismissAlert'>`,
-                `   <div>Live Alert</div>`,
-                '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
-                '</div>'
-            ].join('');
-            alertPlaceholder.append(wrapper);
-        };
-        appendAlert();
-    }
+  liveAlert() {
+    const template = this.alertTemplateTarget;
+    const clone = template.content.cloneNode(true);
+    document.body.appendChild(clone);
+  }
 
-    dismissAlert() {
-        this.bsAlert.close();
-    }
+  dismissAlert() {
+    this.bsAlert.close();
+  }
 }
