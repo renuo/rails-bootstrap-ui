ffe7fd7044d915f1969e6350260cef7874c0d9ab
Eduard Munteanu <ed.munteanu99@gmail.com>
2024-07-10 13:40:16 +0200

    Add controller

---
+++ app/javascript/controllers/tab_controller.js
@@ -0,0 +1,21 @@
+import { Controller } from '@hotwired/stimulus';
+
+export default class extends Controller {
+  static targets = ['tabFieldset'];
+
+  connect() {
+    this.toggleTabFieldsetAttributes();
+  }
+
+  // This method disables fields that are currently hidden in a different tab from the selected one,
+  // therefore preventing them from being submitted by a form.
+  toggleTabFieldsetAttributes() {
+    this.tabFieldsetTargets.forEach((tabFieldset) => {
+      const tabFieldsetVisible = tabFieldset.classList.contains('active');
+      console.log(tabFieldset, tabFieldsetVisible);
+      tabFieldset.disabled = !tabFieldsetVisible;
+      const submitButton = tabFieldset.querySelector('input[type="submit"], input[type="button"]');
+      submitButton.type = tabFieldsetVisible ? 'submit' : 'button';
+    });
+  }
+}
