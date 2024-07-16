6b8f0d66bad993fb6d7573d4901912a3d4d3bf69
Chris <chris.hunziker@renuo.ch>
2024-07-16 12:00:01 +0200

    Breadcrumbs (#12)
    * Add breadcrumbs
    
    * Add pages test

---
+++ app/components/previews/breadcrumbs_component_preview.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class BreadcrumbsComponentPreview < Lookbook::Preview
+  # @source ../../../app/views/components/breadcrumbs/_themes.html.erb
+  def themes
+    render 'components/breadcrumbs/themes'
+  end
+end
