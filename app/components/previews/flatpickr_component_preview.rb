868fc1da059b4fdc99dcbf60c4369b1d0f5b3198
Oliver Anthony <108479068+oliveranthony17@users.noreply.github.com>
2025-02-20 17:59:44 +0100

    Add generated integrity hash for CDNs (#28)
    * Add generated integrity hash for CDNs
    
    * Pin flatpickr version

--- app/components/previews/flatpickr_component_preview.rb
+++ app/components/previews/flatpickr_component_preview.rb
@@ -5,8 +5,16 @@ class FlatpickrComponentPreview < Lookbook::Preview
 
   # Add the following to your `application.html.erb` head:
   #
-  # `<script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>`<br>
-  # `<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">`
+  # `<script
+  #   src="https://cdn.jsdelivr.net/npm/flatpickr@4.6.13/dist/flatpickr.min.js"
+  #   integrity="sha384-5JqMv4L/Xa0hfvtF06qboNdhvuYXUku9ZrhZh3bSk8VXF0A/RuSLHpLsSV9Zqhl6"
+  #   crossorigin="anomynous"></script>`
+  #
+  # `<link
+  #   rel="stylesheet"
+  #   href="https://cdn.jsdelivr.net/npm/flatpickr@4.6.13/dist/flatpickr.min.css"
+  #   integrity="sha384-RkASv+6KfBMW9eknReJIJ6b3UnjKOKC5bOUaNgIY778NFbQ8MtWq9Lr/khUgqtTt%"
+  #   crossorigin="anonymous">`
   #
   # @source ../../../app/views/components/flatpickr/_themes.html.erb
 
